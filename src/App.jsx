bbddd1e188e411de87c1316eea968affc0c764db
Tanishka <makodetanishka@gmail.com>
2023-03-22 13:32:51 +0530

    Add clear all functionality

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,20 @@ import data from "./data";
 import List from "./List";
 
 const App = () => {
-  const [people, setpeople] = useState(data);
+  const [people, setPeople] = useState(data);
 
   return (
     <main>
       <section className="container">
         <h3>{people.length} birthdays today</h3>
         <List people={people} />
+        <button
+          type="button"
+          className="btn btn-block"
+          onClick={() => setPeople([])}
+        >
+          Clear All
+        </button>
       </section>
     </main>
   );
