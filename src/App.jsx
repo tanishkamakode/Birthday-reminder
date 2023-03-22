5bcba0638f4e6a06c4f4a1b8d88602f09025e48b
Tanishka <makodetanishka@gmail.com>
2023-03-22 13:29:42 +0530

    Add birthdays

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import React from "react";
-import Data from "./data";
+import { useState } from "react";
+import data from "./data";
+import List from "./List";
 
 const App = () => {
-  return <h2>Birthday Reminder</h2>;
+  const [people, setpeople] = useState(data);
+
+  return (
+    <main>
+      <section className="container">
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+      </section>
+    </main>
+  );
 };
 export default App;
